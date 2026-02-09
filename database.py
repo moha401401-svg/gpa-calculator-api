5e8b8de139ea90e58c9914a6a407c9d341d4944d
youssef ali <moha401401@gmail.com>
2026-02-09 19:11:43 +0200

    Final version for Railway

--- database.py
+++ database.py
@@ -8,12 +8,15 @@ def database():
         port=int(os.getenv("MYSQLPORT", 3306)),
         database=os.getenv("MYSQLDATABASE")
     )
-db=database()
-cr=db.cursor()
-cr.execute('create database if not exists railway')
-cr.execute('create table if not exists calcs(id int auto_increment primary key,name varchar(25)' \
-',CS int,Math1 int ,Discrete int,Elctronics int,Creative_thinking int' \
-',Technical_writing int,GPA DECIMAL)')
-db.commit()
-cr.close()
-db.close()
+try:
+    db=database()
+    cr=db.cursor()
+    cr.execute('create database if not exists railway')
+    cr.execute('create table if not exists calcs(id int auto_increment primary key,name varchar(25)' \
+    ',CS int,Math1 int ,Discrete int,Elctronics int,Creative_thinking int' \
+    ',Technical_writing int,GPA DECIMAL)')
+    db.commit()
+    cr.close()
+    db.close()
+except Exception as e:
+    print(f'hmmmmm {e}')
