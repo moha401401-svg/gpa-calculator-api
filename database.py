03e8880329aa9918ee006b01e55aff184a0774e8
youssef ali <moha401401@gmail.com>
2026-02-09 20:43:31 +0200

    m

--- database.py
+++ database.py
@@ -1,44 +1,29 @@
-import mysql.connector as my
 import os
+import mysql.connector
+from urllib.parse import urlparse
 
 def database():
-    host = os.getenv("MYSQLHOST")
-    user = os.getenv("MYSQLUSER")
-    password = os.getenv("MYSQLPASSWORD")
-    port = int(os.getenv("MYSQLPORT", 3306))
-    dbname = os.getenv("MYSQLDATABASE")
-
     try:
-        return my.connect(
-            host=host,
-            user=user,
-            password=password,
-            port=port,
-            database=dbname
-        )
+        database_url = os.getenv("DATABASE_URL")
+
+        if not database_url:
+            raise Exception("DATABASE_URL not found")
+
+        parsed = urlparse(database_url)
+
+        conn = mysql.connector.connect(
+        host=parsed.hostname,
+        user=parsed.username,
+        password=parsed.password,
+        database=parsed.path.lstrip("/"),
+        port=parsed.port,
+        ssl_disabled=False
+)
+
+
+        print("✅ Connected to Railway MySQL!")
+        return conn
+
     except Exception as e:
         print(f"❌ Connection Failed: {e}")
         return None
-def initialize_db():
-    db = database()
-    if db:
-        cr = db.cursor()
-        cr.execute('''CREATE TABLE IF NOT EXISTS calcs (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(25),
-            CS INT,
-            Math1 INT,
-            Discrete INT,
-            Elctronics INT,
-            Creative_thinking INT,
-            Technical_writing INT,
-            GPA FLOAT 
-        )''')
-        db.commit()
-        cr.close()
-        db.close()
-        print("✅ Database is ready!")
-
-
-if __name__ == "__main__":
-    initialize_db()
