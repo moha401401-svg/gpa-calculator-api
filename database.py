afc95750ba7a3d46d819563650278ef90b2a6f51
youssef ali <moha401401@gmail.com>
2026-02-11 19:53:21 +0200

    finally!

--- database.py
+++ database.py
@@ -36,13 +36,13 @@ def initialize_db():
         CREATE TABLE IF NOT EXISTS calcs (
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(25),
-            CS FLOAT,
-            Math1 FLOAT,
-            Discrete FLOAT,
-            Elctronics FLOAT,
-            Creative_thinking FLOAT,
-            Technical_writing FLOAT,
-            GPA FLOAT
+            CS DECIMAL(5,2),
+            Math1 DECIMAL(5,2),
+            Discrete DECIMAL(5,2),
+            Elctronics DECIMAL(5,2),
+            Creative_thinking DECIMAL(5,2),
+            Technical_writing DECIMAL(5,2),
+            GPA DECIMAL(5,2)
         )
         ''')
         db.commit()
@@ -57,17 +57,16 @@ def initialize_db2():
         CREATE TABLE IF NOT EXISTS calcs2 (
             id INT AUTO_INCREMENT PRIMARY KEY,
             name VARCHAR(25),
-            CS FLOAT,
-            Discrete FLOAT,
-            Elctronics FLOAT,
-            Creative_thinking FLOAT,
-            Technical_writing FLOAT,
-            GPA FLOAT
+            CS DECIMAL(5,2),
+            Discrete DECIMAL(5,2),
+            Elctronics DECIMAL(5,2),
+            Creative_thinking DECIMAL(5,2),
+            Technical_writing DECIMAL(5,2),
+            GPA DECIMAL(5,2)
         )
         ''')
         db.commit()
         cr.close()
         db.close()
         print("✅ Database is ready!")
-initialize_db2()
 
--- service.py
+++ service.py
@@ -33,12 +33,12 @@ def insert_gpa2(Grades2):
     l=['name','CS','Discrete','Elctronics','Creative_thinking','Technical_writing']
     s=['%s']
     if Grades2.name is not None:
-        query=f"insert into calcs({' ,'.join(l)},GPA) values({','.join(s*7)})"
+        query=f"insert into calcs2({' ,'.join(l)},GPA) values({','.join(s*7)})"
         values=list(getattr(Grades2,col)for col in l)
         values.append(calculated_GPA)
         cr.execute(query,tuple(values))
     else:
-         query=f"insert into calcs({' ,'.join(l[1:])},GPA) values({','.join(s*6)})"
+         query=f"insert into calcs2({' ,'.join(l[1:])},GPA) values({','.join(s*6)})"
          values=list(getattr(Grades2,col) for col in l[1:])
          values.append(calculated_GPA)
          cr.execute(query,tuple(values))
